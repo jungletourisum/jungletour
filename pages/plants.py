b0927eb0b9b84e77403b844edcbded2fbefb1d1a
jungletourisum <134143962+jungletourisum@users.noreply.github.com>
2023-05-20 20:03:10 +0530

    Update plants.py

--- pages/plants.py
+++ pages/plants.py
@@ -9,11 +9,38 @@ model = tf.keras.models.load_model("saved_model/plants.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file")
 
-map_dict = { 0:'turmericfingers',
-             1:'turmericbulbs',
-             2:'sproutedturmeric',
-             3:'insectdamages',
-             4:'healthyrawturmeric',
+map_dict = { 0:'Black-Honey Shrub - Phyllanthus reticulatus',
+             1:'Butterfly Pea - Clitoria ternatea',
+             2:'Carissa Carandas (Karanda)',
+             3:'Jasminum (Jasmine)',
+             4:'kohomba - Azadirachta indica',
+             5:'Mountain Knotgrass - Aerva lanata',
+             6:'Muntingia Calabura (Jamaica Cherry-Gasagase)',
+             7:'Prickly Chaff Flower - Achyranthes aspera',
+             8:'Purple Fruited Pea Eggplant - Solanum trilobatum',
+             9:'Purple Tephrosia - Tephrosia purpurea',
+             10:'Balloon vine - Cardiospermum halicacabum',
+             11:'Bellyache bush (Green) - Jatropha gossypiifolia',
+             12:'blue porterweed - Stachytarpheta jamaicensis',
+             13:'coatbuttons - Tridax procumbens',
+             14:'Ivy Gourd - Coccinia grandis',
+             15:'Santalum Album (Sandalwood)',
+             16:'Shaggy button weed - Spermacoce hispida',
+             17:'Velvet bean- Mucuna pruriens',
+             18:'Wild Pepper - Solanum bahamense',
+             19:'Ceylon satinwood (Chloroxylon swietenia)',
+             20:'Hourglass tree (Humboldtia laurifolia)',
+             21:'Serendib scaly tree fern (Cyathea contaminans)',
+             22:'Sinharaja damba (Grewia damine)',
+             23:'Boralu - Anisomeles malabarica',
+             24:'Gammalu - Diospyros ovalifolia',
+             25:'Divul - Diospyros malabarica',
+             26:'Dun - Syzygium caryophyllatum',
+             27:'Hora - Dipterocarpus zeylanicus',
+             28:'Kahata - Ficus microcarpa',
+             29:'Kekuna -Calophyllum inophyllum',
+             30:'Kothala Himbutu - Salacia reticulata',
+             31:'Meewana - Memecylon edule
             }
 
 
