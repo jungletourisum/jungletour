ebd025bcecf92a4f38890913fb0f57a31d75dbee
jungletourisum <134143962+jungletourisum@users.noreply.github.com>
2023-09-05 14:07:17 +0530

    Update reptile.py

--- pages/reptile.py
+++ pages/reptile.py
@@ -45,7 +45,8 @@ if uploaded_file is not None:
     if Genrate_pred:
         prediction = model.predict(img_reshape).argmax()
         if prediction == 0 :
-           st.text("Description The Black-lipped Lizard (Calotes nigrilabris) is a reptile species known for its striking appearance and unique features. With an average size of 30-40 centimeters, it showcases a vibrant combination of green and black coloration, complemented by a distinct black stripe on its upper lip. This lizard is classified as a species of least concern in terms of conservation status")
-        else:
-            st.title("else")            
+                    st.text("Description The Black-lipped Lizard (Calotes nigrilabris) is a reptile species known for its striking appearance and unique features. With an average size of 30-40 centimeters, it showcases a vibrant combination of green and black coloration, complemented by a distinct black stripe on its upper lip. This lizard is classified as a species of least concern in terms of conservation status")
+        if prediction == 10 :
+                    st.text("Description : The Hump-nosed Viper (Hypnale spp.) is a venomous snake species found in the Sinharaja Forest. The Hump-nosed Viper is a medium-sized snake, averaging around 50-70 centimeters in length. It has a distinctive triangular-shaped head with a prominent hump on its nose. This species showcases various color variations, including shades of brown, gray, or olive, often accompanied by intricate patterns of zigzag lines or blotches along its body, aiding in its identification. It possesses keeled scales and vertical pupils")
+                    
         st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
