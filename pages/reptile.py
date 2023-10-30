673e8f317325359db0354069d3bd9999cf13d1e5
jungletourisum <134143962+jungletourisum@users.noreply.github.com>
2023-10-31 04:52:43 +0530

    Update reptile.py

--- pages/reptile.py
+++ pages/reptile.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
